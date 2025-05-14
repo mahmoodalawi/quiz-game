93590c61b362af5078d156586b526f593e749fa2
mahmoodalawi <mahmoodbh19@email.com>
2025-05-14 17:55:06 +0300

    Simple Array that contain 3 question as start

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const quizlist = [
+    {
+        question: "What is the maximum probation period allowed in Bahrain?",
+        answers: ["3 months", "6 months", "1 year", "No limit"],
+        correct: "3 months"
+    },
+    {
+        question: "How many days of annual leave in Bahrain Labour Law?",
+        answers: ["100 days", "28 days", "30 days"],
+        correct: "30 days"
+    },
+    {
+        question: "What is the minimum basic salary for workers in Bahrain as per the Labour Law?",
+        answers: ["BD 1000", "BD 200", "BD 350"],
+        correct: "BD 350"
+    }
+  ];
 /*-------------------------------- Variables --------------------------------*/
+
 
 /*------------------------ Cached Element References ------------------------*/
 
